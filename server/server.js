5e7303b52241d77a82f3d9e08444fdc8ecfa1091
Le Ngoc Minh <lnminh58@gmail.com>
2018-10-31 11:35:07 +0700

    modify

--- server/server.js
+++ server/server.js
@@ -86,7 +86,7 @@ io.on('connection', socket => {
       io.to(user.room).emit('updateUserList', users.getUserList(user.room));
       io.to(user.room).emit(
         'newMessage',
-        generateMessage('Admin', `${user.name} has left`)
+        generateMessage('ADMIN', `${user.name} has left`)
       );
     }
   });
